a621a8e00810817a8ef93655945635fe50dffca2
Pete Hamrick <pete.hamrick@gmail.com>
2021-08-31 21:57:55 -0700

    stop and delete recurring purchases working

--- src/Components/User.js
+++ src/Components/User.js
@@ -36,7 +36,7 @@ class User extends Component {
                 </div>
                 <NavLink to='/addpurchaseitem'>Add New Expense</NavLink> 
                 <NavLink to='/addrecurringpurchaseitem'>Add Recurring Expense</NavLink>
-
+                <NavLink to='/modifyrecurringpurchaseitem'>Modify Recurring Expense</NavLink>
             </>
          );
     }
