053cf71e591f7e848cba37ca9aaef9dc41466839
Paul Brubaker <pbrubak@gmail.com>
2021-11-02 15:27:55 -0700

    fix normalize cost helper function

--- src/helper-functions.js
+++ src/helper-functions.js
@@ -1,72 +1,84 @@
 export function findById(array, id) {
-    for (let item of array) {
-        if (item.id === id) {
-            return item
-        }
+  for (let item of array) {
+    if (item.id === id) {
+      return item;
     }
+  }
 }
 
 export function findByParentId(arr, id) {
-    const result = [];
-    arr.forEach(item => {
-        if (item.parent_id === id) {
-            result.push(item);
-        }
-    });
-    return result;
-}
-
-export function getParentId(categoriesArr, childId) {
-    let result;
-    categoriesArr.forEach(category => {
-        if (category.id === childId) {
-            result = category.parent_id;
-        } 
-    })
-    return result;
+  const result = [];
+  arr.forEach((item) => {
+    if (item.parent_id === id) {
+      result.push(item);
+    }
+  });
+  return result;
 }
 
 export function normalizeCost(start, stop, now, timeWindow, frequency, cost) {
-    let costResult;
-        if (start > now - timeWindow) {
-            if (stop) {
-                costResult = (cost / frequency) * (stop - start);
-            } else {
-                costResult = (cost / frequency) * (now - start);
-            }
-        } else {
-            if (stop) {
-                costResult = (cost / frequency) * (stop - (now - timeWindow));
-            } else {
-                costResult = (cost / frequency) * (now - (now -timeWindow));
-            }
-        }
-        return `$${costResult}`;
+  let costResult;
+  if (start > now - timeWindow) {
+    if (stop) {
+      costResult = (cost / frequency) * (stop - start);
+    } else {
+      costResult = (cost / frequency) * (now - start);
     }
+  } else {
+    if (stop) {
+      costResult = (cost / frequency) * (stop - (now - timeWindow));
+    } else {
+      costResult = (cost / frequency) * (now - (now - timeWindow));
+    }
+  }
+  return `$${costResult}`;
+}
 
-export function getSumRecursively(purchasesArr, categoryId) {
-    let sum = 0;
-    purchasesArr.forEach(purchase => {
-        if (purchase.parent_id === categoryId) {
-            sum += Number(purchase.normalizedCost.slice(1));
-            const childArr = findByParentId(purchasesArr, purchase.category_id);
-            if (childArr.length !== 0) {
-                sum += getSumRecursively(childArr, purchase.id);
-            }
-        } else if (purchase.category_id === categoryId) {
-            sum += Number(purchase.normalizedCost.slice(1));
+export function getSumRecursively(purchasesArr, categoryId, allCategories) {
+  const descendantCategories = [];
+
+  const getDescendantCategories = (parentId) => {
+    allCategories.forEach((category) => {
+      if (category.parent_id === parentId) {
+        if (
+          allCategories.filter((item) => item.parent_id === category.id)
+            .length === 0
+        ) {
+          descendantCategories.push(category.id);
+        } else {
+          getDescendantCategories(category.id);
         }
+      }
     });
-    return sum;
+  };
+
+  getDescendantCategories(categoryId);
+  let sum = 0;
+
+  if (!descendantCategories.length) {
+    purchasesArr
+      .filter((purchase) => purchase.category_id === categoryId)
+      .forEach((purchase) => {
+        sum += Number(purchase.normalizedCost.slice(1));
+      });
+  } else {
+    purchasesArr.forEach((purchase) => {
+      if (descendantCategories.includes(purchase.category_id)) {
+        sum += Number(purchase.normalizedCost.slice(1));
+      }
+    });
+  }
+  return sum;
 }
 
 export function mungeChartData(categoriesArr, allCategories, purchasesArr) {
-    const result = {};
-    const mungedPurchases = purchasesArr.map(purchase => {
-        return { parent_id: getParentId(allCategories, purchase.category_id),...purchase }
-    });
-    categoriesArr.forEach(category => {
-        result[category.description] =  getSumRecursively(mungedPurchases, category.id);
-    });
-    return result;
-}   
+  const result = {};
+  categoriesArr.forEach((category) => {
+    result[category.description] = getSumRecursively(
+      purchasesArr,
+      category.id,
+      allCategories
+    );
+  });
+  return result;
+}
