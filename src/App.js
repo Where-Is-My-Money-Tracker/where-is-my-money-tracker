024d8125aae47ac5652c6cb457b80a8fc490b8f1
Pete Hamrick <pete.hamrick@gmail.com>
2021-11-10 16:38:00 -0800

    added comments in app.js for path change suggestions

--- src/App.js
+++ src/App.js
@@ -1,106 +1,130 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import AddPurchaseItem from './Components/AddPurchaseItemForm.js';
-import AddRecurringPurchaseItem from './Components/AddRecurringPurchaseItem.js';
-import ModifyRecurringPurchaseItem from './Components/ModifyRecurringPurchaseItem.js';
-import Signin from './Components/SignInForm.js';
-import Signup from './Components/SignUpForm.js';
-import Header from './Components/Header.js';
-import Home from './Components/Home.js';
-import AboutUs from './Components/AboutUs.js';
-import User from './Components/User.js';
-import DeletePurchases from './Components/DeletePurchases.js';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import AddPurchaseItem from "./Components/AddPurchaseItemForm.js";
+import AddRecurringPurchaseItem from "./Components/AddRecurringPurchaseItem.js";
+import ModifyRecurringPurchaseItem from "./Components/ModifyRecurringPurchaseItem.js";
+import Signin from "./Components/SignInForm.js";
+import Signup from "./Components/SignUpForm.js";
+import Header from "./Components/Header.js";
+import Home from "./Components/Home.js";
+import AboutUs from "./Components/AboutUs.js";
+import User from "./Components/User.js";
+import DeletePurchases from "./Components/DeletePurchases.js";
+import "./App.css";
 
 class App extends Component {
-  state = { 
-    token: localStorage.getItem('TOKEN'),
-   }
-   setToken = (value) => {
-     this.setState({token: value})
-   }
-  render() { 
-    return ( 
-      <BrowserRouter>
-      <Header/>
-      <Switch>
-        <Route path="/signin" 
-                render={(routerProps) => (
-                  <Signin
-                  setToken={this.setToken}
-                  {...routerProps}/>
-                )}>
-        </Route>
-        <Route path="/signup" 
-                render={(routerProps) => (
-                  <Signup
-                  setToken={this.setToken}
-                  {...routerProps}/>
-                )}>
-        </Route>
-        <Route path="/user"
-                render={(routerProps) => (
-                  this.state.token ?
-                  <User
-                  token={this.state.token}
-                  {...routerProps}/>
-                  : <Redirect to='/' />
-                )}>
-        </Route>
-        <Route path="/addpurchaseitem" 
-                render={(routerProps) => (
-                  this.state.token ?
-                  <AddPurchaseItem
-                  token={this.state.token}
-                  {...routerProps}/>
-                  : <Redirect to='/' />
-                )}>
-        </Route>
-        <Route path="/addrecurringpurchaseitem" 
-                render={(routerProps) => (
-                  this.state.token ?
-                  <AddRecurringPurchaseItem
-                  token={this.state.token}
-                  {...routerProps}/>
-                  : <Redirect to='/' />
-                )}>
-        </Route>
-        <Route path="/modifyrecurringpurchaseitem" 
-                render={(routerProps) => (
-                  this.state.token ?
-                  <ModifyRecurringPurchaseItem
-                  token={this.state.token}
-                  {...routerProps}/>
-                  : <Redirect to='/' />
-                )}>
-        </Route>
-        <Route path="/deletepurchases" 
-                render={(routerProps) => (
-                  this.state.token ?
-                  <DeletePurchases
-                  token={this.state.token}
-                  {...routerProps}/>
-                  : <Redirect to='/' />
-                )}>
-        </Route>
-        <Route path="/aboutus"
-          render={(routerProps) => (
-            <AboutUs
-            {...routerProps}/>
-          )}>
-        </Route>
-        <Route exact path="/" 
-        render={(routerProps) => (
-          <Home
-          token={this.state.token}
-          setToken={this.setToken}
-          {...routerProps}/>
-        )}>  
-        </Route>
-      </Switch>
-    </BrowserRouter>
-     );
-  }
+    state = {
+        token: localStorage.getItem("TOKEN"),
+    };
+    setToken = (value) => {
+        this.setState({ token: value });
+    };
+    render() {
+        return (
+            <BrowserRouter>
+                <Header />
+                <Switch>
+                    <Route
+                        path="/signin"
+                        render={(routerProps) => (
+                            <Signin setToken={this.setToken} {...routerProps} />
+                        )}
+                    ></Route>
+                    <Route
+                        path="/signup"
+                        render={(routerProps) => (
+                            <Signup setToken={this.setToken} {...routerProps} />
+                        )}
+                    ></Route>
+                    {/* change path below to /purchases */}
+                    <Route
+                        path="/user"
+                        render={(routerProps) =>
+                            this.state.token ? (
+                                <User
+                                    token={this.state.token}
+                                    {...routerProps}
+                                />
+                            ) : (
+                                <Redirect to="/" />
+                            )
+                        }
+                    ></Route>
+                    {/* change path below to /purchases/add */}
+                    <Route
+                        path="/addpurchaseitem"
+                        render={(routerProps) =>
+                            this.state.token ? (
+                                <AddPurchaseItem
+                                    token={this.state.token}
+                                    {...routerProps}
+                                />
+                            ) : (
+                                <Redirect to="/" />
+                            )
+                        }
+                    ></Route>
+                    {/* change path below to /purchases/add */}
+                    <Route
+                        path="/addrecurringpurchaseitem"
+                        render={(routerProps) =>
+                            this.state.token ? (
+                                <AddRecurringPurchaseItem
+                                    token={this.state.token}
+                                    {...routerProps}
+                                />
+                            ) : (
+                                <Redirect to="/" />
+                            )
+                        }
+                    ></Route>
+                    {/* change path below to /purchases/modify */}
+                    <Route
+                        path="/modifyrecurringpurchaseitem"
+                        render={(routerProps) =>
+                            this.state.token ? (
+                                <ModifyRecurringPurchaseItem
+                                    token={this.state.token}
+                                    {...routerProps}
+                                />
+                            ) : (
+                                <Redirect to="/" />
+                            )
+                        }
+                    ></Route>
+                    {/* change path below to /purchases/delete */}
+                    <Route
+                        path="/deletepurchases"
+                        render={(routerProps) =>
+                            this.state.token ? (
+                                <DeletePurchases
+                                    token={this.state.token}
+                                    {...routerProps}
+                                />
+                            ) : (
+                                <Redirect to="/" />
+                            )
+                        }
+                    ></Route>
+                    <Route
+                        path="/aboutus"
+                        render={(routerProps) => <AboutUs {...routerProps} />}
+                    ></Route>
+                    <Route
+                        exact
+                        path="/"
+                        render={(routerProps) => (
+                            <Home
+                                token={this.state.token}
+                                setToken={this.setToken}
+                                {...routerProps}
+                            />
+                        )}
+                    ></Route>
+                </Switch>
+            </BrowserRouter>
+        );
+    }
 }
- 
+
 export default App;
--- src/Components/AddPurchaseItemForm.js
+++ src/Components/AddPurchaseItemForm.js
@@ -1,100 +1,141 @@
-import React, { Component } from 'react';
-import { getCategories, postCategories, postPurchase } from '../fetch-utils.js';
-import { findById } from '../helper-functions.js';
-import '../Styles/AddPurchaseItemForm.css';
+import React, { Component } from "react";
+import { getCategories, postCategories, postPurchase } from "../fetch-utils.js";
+import { findById } from "../helper-functions.js";
+import "../Styles/AddPurchaseItemForm.css";
 
 class AddPurchaseItem extends Component {
-    state = { 
-        parentCategory:0,
-        childCategories:[],
-        cost:'',
-        description:'', 
-        timestamp:0,
-        newCategoryInput:'',
-        optionSelector:'--',
-        allCategories: []
-     }
+    state = {
+        parentCategory: 0,
+        childCategories: [],
+        cost: "",
+        description: "",
+        timestamp: 0,
+        newCategoryInput: "",
+        optionSelector: "--",
+        allCategories: [],
+    };
 
-     componentDidMount = async() => {
-        const categories = await getCategories()  
-        this.setState({allCategories: categories})
-        const childCategories = categories.filter(item => item.parent_id === this.state.parentCategory)
-        this.setState({childCategories})
-     }
+    componentDidMount = async () => {
+        const categories = await getCategories();
+        this.setState({ allCategories: categories });
+        const childCategories = categories.filter(
+            (item) => item.parent_id === this.state.parentCategory
+        );
+        this.setState({ childCategories });
+    };
+
     handleChange = async (e, key) => {
         this.setState({ [key]: e.target.value });
-    }
+    };
+
     handleCategoryChange = async (e) => {
-        this.setState({optionSelector: e.target.value})
-        if (e.target.value !== 'SetNewCategory') {
+        this.setState({ optionSelector: e.target.value });
+        if (e.target.value !== "SetNewCategory") {
             this.setState({ parentCategory: e.target.value });
-        } 
+        }
         const categories = await getCategories();
-        const childCategories = categories.filter(item => item.parent_id === Number(e.target.value));
+        const childCategories = categories.filter(
+            (item) => item.parent_id === Number(e.target.value)
+        );
         this.setState({ childCategories });
-    }
-    handleSubmitPurchase = async(e) => {
-        e.preventDefault()
+    };
+
+    handleSubmitPurchase = async (e) => {
+        e.preventDefault();
         const newPurchase = {
             description: this.state.description,
             cost: Number(this.state.cost) * 100,
             category_id: this.state.parentCategory,
-            timestamp: Date.now() 
-        }
-        if (this.state.optionSelector === 'SetNewCategory'){
+            timestamp: Date.now(),
+        };
+        if (this.state.optionSelector === "SetNewCategory") {
             const newCategory = await postCategories({
-                 parent_id: Number(this.state.parentCategory),
-                 description: this.state.newCategoryInput
-            })
-            newPurchase.category_id = newCategory.id
+                parent_id: Number(this.state.parentCategory),
+                description: this.state.newCategoryInput,
+            });
+            newPurchase.category_id = newCategory.id;
         }
-         await postPurchase(newPurchase)
-         this.props.history.push('/user')
-    }
-   render() {
+        await postPurchase(newPurchase);
+        this.props.history.push("/user");
+    };
+
+    render() {
         let labelMessage;
-        if(Number(this.state.parentCategory) === 0){
-            labelMessage = 'Main';
+        if (Number(this.state.parentCategory) === 0) {
+            labelMessage = "Main";
         } else {
-            labelMessage = findById(this.state.allCategories, Number(this.state.parentCategory).description)
+            labelMessage = findById(
+                this.state.allCategories,
+                Number(this.state.parentCategory).description
+            );
         }
-        return ( 
+        return (
             <div className="addPurchaseForm">
                 <h1>Add New Purchase</h1>
-                <form onSubmit={(e)=> this.handleSubmitPurchase(e)} className="purchaseInput">
+                <form
+                    onSubmit={(e) => this.handleSubmitPurchase(e)}
+                    className="purchaseInput"
+                >
                     <p>Item Description</p>
-                    <input type='text'
-                        onChange={(e)=> this.setState({description: e.target.value})}
-                        required></input>
+                    <input
+                        type="text"
+                        onChange={(e) =>
+                            this.setState({ description: e.target.value })
+                        }
+                        required
+                    ></input>
                     <p>Cost</p>
-                    <input type='number'
-                        step='0.01'
-                        onChange={(e)=> this.setState({cost: e.target.value})}
-                        required>
-                    </input>
+                    <input
+                        type="number"
+                        step="0.01"
+                        onChange={(e) =>
+                            this.setState({ cost: e.target.value })
+                        }
+                        required
+                    ></input>
                     <br></br>
-                    <label id="dropDownLabel">Current Category: {labelMessage}
+                    <label id="dropDownLabel">
+                        Current Category: {labelMessage}
                         <br></br>
-                        <p><i>Use the selector below to choose a subcategory of, or add a new subcategory to, {labelMessage}</i></p>
+                        <p>
+                            <i>
+                                Use the selector below to choose a subcategory
+                                of, or add a new subcategory to, {labelMessage}
+                            </i>
+                        </p>
                     </label>
-                    <select onChange={(e) => this.handleCategoryChange(e)}
-                            value={this.state.optionSelector}>
-                            <option value='--'>--</option>
-                        {this.state.childCategories.map(item => (
-                            <option key={item.id} value={item.id}>{item.description}</option>
-                            ))}
-                            <option key='new' value='SetNewCategory'>Add New Category</option>
+                    <select
+                        onChange={(e) => this.handleCategoryChange(e)}
+                        value={this.state.optionSelector}
+                    >
+                        <option value="--">--</option>
+                        {this.state.childCategories.map((item) => (
+                            <option key={item.id} value={item.id}>
+                                {item.description}
+                            </option>
+                        ))}
+                        <option key="new" value="SetNewCategory">
+                            Add New Category
+                        </option>
                     </select>
-                    {this.state.optionSelector === 'SetNewCategory' || this.state.childCategories.length === 0
-                        ? <input type='text'
-                            onChange={(e)=> this.setState({newCategoryInput: e.target.value})}>
-                            </input> 
-                        : <p></p>} 
+                    {this.state.optionSelector === "SetNewCategory" ||
+                    this.state.childCategories.length === 0 ? (
+                        <input
+                            type="text"
+                            onChange={(e) =>
+                                this.setState({
+                                    newCategoryInput: e.target.value,
+                                })
+                            }
+                        ></input>
+                    ) : (
+                        <p></p>
+                    )}
                     <button id="submit-button">Submit</button>
                 </form>
             </div>
-         );
+        );
     }
 }
- 
+
 export default AddPurchaseItem;
